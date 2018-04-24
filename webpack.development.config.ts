6de7069e5a67c6c7d255022148bf595792f687ce
Heorhii <h30rhii@gmail.com>
2018-04-24 17:47:59 +0300

    Splitted webpack configuration to two different files and excluded jest configuration from package.json

--- webpack.development.config.ts
+++ webpack.development.config.ts
@@ -2,6 +2,7 @@ import * as path from 'path';
 import * as webpack from 'webpack';
 
 const config: webpack.Configuration = {
+  mode: 'development',
   resolve: {
 	extensions: ['.ts', '.tsx', '.js', '.json']
   },
---
+++ webpack.production.config.ts
@@ -0,0 +1,33 @@
+import * as path from 'path';
+import * as webpack from 'webpack';
+
+const config: webpack.Configuration = {
+  mode: 'production',
+  resolve: {
+	extensions: ['.ts', '.tsx', '.js', '.json']
+  },
+  entry: './src/index.tsx',
+  output: {
+	path: path.resolve(__dirname, 'dist'),
+	filename: 'bundle.js'
+  },
+  module: {
+	rules: [
+	  {
+		test: /\.tsx?$/,
+		loader: 'awesome-typescript-loader'
+	  },
+	  {
+		enforce: 'pre',
+		test: /\.js$/,
+		loader: 'source-map-loader'
+	  }
+	]
+  },
+  externals: {
+	'react': 'React',
+	'react-dom': 'ReactDOM'
+  }
+};
+
+export default config;
